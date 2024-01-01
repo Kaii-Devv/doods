235b900bffec175c3666ad55615f313bbb38faec
cees <ceeskamu@gmail.com>
2024-01-02 00:00:07 +0700

    x

--- main.py
+++ main.py
@@ -1,46 +1,43 @@
 # main
 from flask import Flask, send_file, request, jsonify, render_template, Response
-import requests, re, io, magic,os
+import requests, re, io,os
 import random
 import numpy as np
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor,as_completed
 
 app = Flask(__name__)
 
 
-proxynp={}
-def dump():
-  proxy = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=socks4&timeout=10000&country=all&ssl=all&anonymity=all').textq
+def dump(tok):
+  proxy = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=socks4&timeout=10000&country=all&ssl=all&anonymity=all').text
   proxies = np.char.replace(proxy.split('\n')[:-1],'\r','')
-  result={}
   with ThreadPoolExecutor(max_workers=500) as pool:
-    for proxy in proxies:
-      try:
-          result = pool.submit(check, proxy)
-          if result not False:
-              break
-      except Exception as e:pass
-  return result
+    futures = [pool.submit(check, proxy, tok) for proxy in proxies]
+    for future in as_completed(futures):
+        result = future.result()
+        if result
+        
 
-def check(proxy):
-  global proxynp
+def check(proxy,tok):
   proxy = {
     'http': 'socks4://'+proxy,
     'https': 'socks4://'+proxy
 }
   try:
     ses=requests.Session()
-    host= 'https://ds2play.com'
+    host= 'https://d0o0d.com'
     ses.proxies.update(proxy)
-    tok=request.args.get('token')
-    log2=ses.get(host+"/e/"+tok,headers={"Host": host.replace('https://',''),"referer": host+"/e/"+tok,"accept-encoding": "gzip","cookie": "lang=1; referer=","user-agent": "okhttp/4.9.0"})
+    log2=ses.get(host+"/e/"+tok,headers={'Host': 'd0o0d.com', 'referer': 'https://d0o0d.com/e/', 'accept-encoding': 'gzip', 'user-agent': 'okhttp/4.9.0'},timeout=10)
     if not 'ddos' in log2.text.lower():
         link=host+"/pass_md5/"+re.search("/pass_md5/(.*?)', function",str(log2.text)).group(1)
-        result = ses.get(link,headers={"Host": host.replace('https://',''),"referer": log2.url,"accept-encoding": "gzip","cookie": "lang=1; referer=","user-agent": "okhttp/4.9.0"}).text+"".join([random.choice('abcdefghijklmnopqrstuvwxyz1234567890') for _ in range(10)])+"?token="+link.split("/")[-1]+"&expiry=1"+"".join([str(random.randrange(1,9)) for _ in range(12)])
-        return {'result': result,'IP':proxy['http'].replace('socks4://','')}
-    else:return False
-  except Exception as e :return False
-
+        result = ses.get(link,headers={"Host": host.replace('https://',''),"referer": log2.url,"accept-encoding": "gzip","cookie": "lang=1","user-agent": "okhttp/4.9.0"}).text+"".join([random.choice('abcdefghijklmnopqrstuvwxyz1234567890') for _ in range(10)])+"?token="+link.split("/")[-1]+"&expiry=1"+"".join([str(random.randrange(1,9)) for _ in range(12)])
+        videos = ses.get(link,headers={'Range': 'bytes=0-', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) coc_coc_browser/83.0.144 Chrome/77.0.3865.144 Safari/537.36', 'Referer': 'https://dooood.com/', 'Host': 'no951gt.video-delivery.net', 'Connection': 'Keep-Alive', 'Accept-Encoding': 'gzip'},stream=True)
+        return videos
+        #myresult = {'result': result,'IP':proxy['http']}
+        # bagaimana agar saya bisa me return myresult ke user
+        # dan jika tidak ada error maka ThreadPoolExecutor akan berhenti
+    else:pass
+  except Exception as e :pass
 @app.route('/d/')
 def unduh():
   log2=""
@@ -48,7 +45,7 @@ def unduh():
     tok=request.args.get('token')
     if tok:pass
     else:return {'return':'need params token'}
-    return dump()
+    dump(tok)
   except Exception as e:
     return {'result':str(e)}
 
@@ -72,13 +69,10 @@ def index():
 def read():
   link = request.args.get('link')
   ip = request.args.get('ip')
-  print(request.remote_addr)
+  print(request.args)
   if link and ip:
-    result = requests.get(link,headers={'Accept': '*/*', 'Accept-Encoding': 'deflate,gzip', 'referer': 'https://doodstream.com', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36', 'Host': re.search('https://(.*?)/',link, 'Connection': 'Keep-Alive','X-Forwarded-For':ip},stream=True)
+    result = requests.get(link,headers={'Range': 'bytes=0-', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) coc_coc_browser/83.0.144 Chrome/77.0.3865.144 Safari/537.36', 'Referer': 'https://dooood.com/', 'Host': 'no951gt.video-delivery.net', 'Connection': 'Keep-Alive', 'Accept-Encoding': 'gzip','X-Forwarded-For':ip},stream=True)
     return result.raw
   else:return {'return':'need params link'}
-#dump()
-app.run(host='0.0.0.0', port=81)
-#dump()
-#print(proxynp)
-#unduh('ollbtbz0n0u7')
+app.run(port=8888,debug=True)
+
